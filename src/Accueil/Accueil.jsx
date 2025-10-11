5b112c99161093b0e8e9d3199f3d360a5e40f765
adnane.msl <msillaneadnane05@gmail.com>
2025-10-11 01:20:38 +0100

    third commit

--- src/Accueil/Accueil.jsx
+++ src/Accueil/Accueil.jsx
@@ -3,13 +3,13 @@ import './Accueil.css'
 
 function Accueil() {
     return (
-        <div className='accueil page'>
+        <div className='accueil'>
             <div className='accueil-titre'>
                 <h3>Hello World<span>!</span></h3>
                 <h1>adnane msillane</h1>
                 <h2>developpeur front-end</h2>
             </div>
-            <div className='accueil-description'>
+            <div className='accueil-description page'>
                 <p>Développeur Front-End passionné par la création d’interfaces modernes et intuitives. Mon objectif est de transformer des concepts en expériences utilisateur fluides et performantes, en combinant design et technologies web innovantes.</p>
             </div>
         </div>
